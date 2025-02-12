6330a6bb2ed3c39d05e07158a7451db1cc61d292
phil <127081830+philtebi10@users.noreply.github.com>
2025-02-12 16:14:37 -0500

    new

--- variables.tf
+++ variables.tf
@@ -1,5 +1,13 @@
-variable "ssh_key_name" {
-  description = "The name of the SSH key pair to use for instances"
+# AWS Region to create resources in
+variable "aws_region" {
+  description = "AWS Region for EKS cluster"
   type        = string
-  default     = "PCpem"
+  default     = "us-east-1"  # Replace with your desired region
+}
+
+# Name of the EKS cluster
+variable "EKS_TF" {
+  description = "The name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"  # Replace with your preferred cluster name
 }
