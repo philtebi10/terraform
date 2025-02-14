285c9752dbf7fb9f57246b082303edc08a428de6
phil <127081830+philtebi10@users.noreply.github.com>
2025-02-14 09:47:55 -0500

    EKS

--- variables.tf
+++ variables.tf
@@ -1,13 +1,55 @@
-# AWS Region to create resources in
+# variables.tf
 variable "aws_region" {
-  description = "AWS Region for EKS cluster"
+  description = "AWS region where EKS is deployed"
   type        = string
-  default     = "us-east-1"  # Replace with your desired region
 }
 
-# Name of the EKS cluster
-variable "EKS_TF" {
-  description = "The name of the EKS cluster"
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
   type        = string
-  default     = "my-eks-cluster"  # Replace with your preferred cluster name
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for the EKS cluster"
+  type        = list(string)
+}
+
+variable "node_group_name" {
+  description = "Name of the EKS node group"
+  type        = string
+}
+
+variable "instance_types" {
+  description = "Instance types for the worker nodes"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "desired_capacity" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "max_capacity" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
+
+variable "min_capacity" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "allowed_ingress_cidrs" {
+  description = "Allowed CIDR blocks for security group ingress"
+  type        = list(string)
+  default     = ["192.168.1.0/24"]
 }
